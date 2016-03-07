14320a6d26cd84082859e53cf51edbe048cea4a5
Franktian <tianyawen201209@hotmail.com>
2016-03-06 19:26:57 -0500

    Save changes

--- evalAlign.m
+++ evalAlign.m
@@ -13,21 +13,20 @@ fn_AM        = 'am30k';
 lm_type      = '';
 % delta        = TODO;
 % vocabSize    = TODO; 
-numSentences = {1000, 10000, 15000, 30000};
+numSentences = 30000;
 maxIt = 20;
-task5_f = '/h/u1/cs401/A2_SMT/data/Hansard/Testing/Task5.f';
-task5_e = '/h/u1/cs401/A2_SMT/data/Hansard/Testing/Task5.e';
+task5_f = '/u/cs401/A2_SMT/data/Hansard/Testing/Task5.f';
+task5_e = '/u/cs401/A2_SMT/data/Hansard/Testing/Task5.e';
+task5_g = '/u/cs401/A2_SMT/data/Hansard/Testing/Task5.google.e';
 
 % Train your language models. This is task 2 which makes use of task 1
 % LME = lm_train( trainDir, 'e', fn_LME );
 % LMF = lm_train( trainDir, 'f', fn_LMF );
+% LM = LME;
 load(fn_LME, '-mat', 'LM');
 
 % Train your alignment model of French, given English
-% AMs = {};
-% for i=1:length(numSentences)
-%     AMs{i} = align_ibm1( trainDir, numSentences{i}, maxIt, 'file');
-% end
+% AM = align_ibm1( trainDir, numSentences, maxIt, 'file');
 load(fn_AM, '-mat', 'AM');
 
 % ... TODO: more 
@@ -35,16 +34,19 @@ load(fn_AM, '-mat', 'AM');
 % TODO: a bit more work to grab the English and French sentences. 
 %       You can probably reuse your previous code for this
 french_sens = textread(task5_f, '%s', 'delimiter', '\n');
-
-for l=1:length(french_sens)
-    disp(french_sens{l});
-    f = preprocess(french_sens{l}, 'f');
-    e = decode(f, LM, AM, '');
-    disp(e);
-end
+english_sens = textread(task5_e, '%s', 'delimiter', '\n');
+google_sens = textread(task5_g, '%s', 'delimiter', '\n');
 
 % Decode the test sentence 'fre'
-%eng = decode( fre, LME, AMFE, 'smooth', delta, vocabSize );
+for l=1:length(french_sens)
+    disp('*****************');
+    disp(french_sens{l});
+    %f = preprocess(french_sens{l}, 'f');
+    %e = decode(f, LM, AM, '');
+    disp(english_sens{l});
+    disp(google_sens{l});
+end
+
 
 % TODO: perform some analysis
 % add BlueMix code here 
